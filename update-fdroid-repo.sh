e07222893000267b3ea0505d39d0da59910dc6ce
Klas Holmgren <klas0holmgren@gmail.com>
2025-07-06 08:23:16 +0200

    Add version 1.3.0 (Code: 112) APK

--- update-fdroid-repo.sh
+++ update-fdroid-repo.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "[1/5] Running fdroid update..."
+echo "[1/6] Running fdroid update..."
 fdroid update --create-metadata
 
-echo "[2/5] Adding changes in repo/ and metadata/..."
+echo "[2/6] Copying updated files to public repo directory..."
+cp -r repo/* fdroid/repo/
+
+echo "[3/6] Adding changes in repo/ and metadata/..."
 git add repo/ metadata/
 
-echo "[3/5] Committing changes..."
+echo "[4/6] Adding changes in public repo directory..."
+git add fdroid/repo/
+
+echo "[5/6] Committing changes..."
 git commit -m "Update F-Droid repo index and metadata [automated]" || echo "No changes to commit."
 
-echo "[4/5] Pushing to remote..."
+echo "[6/6] Pushing to remote..."
 git push
 
-echo "[5/5] Done! F-Droid repo index and metadata updated and pushed." 
+echo "Done! F-Droid repo index and metadata updated and pushed." 
